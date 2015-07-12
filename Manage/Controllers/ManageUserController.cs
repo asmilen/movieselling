2d70ef74955952508eb4a5f781ef9566be1bfedb
anhnn00797 <anhnn00797@fpt.edu.vn>
2015-07-12 14:07:07 +0700

    sua edit

--- Manage/Controllers/ManageUserController.cs
+++ Manage/Controllers/ManageUserController.cs
@@ -221,10 +221,76 @@ namespace Manage.Controllers
             return RedirectToAction("List");
         }
 
+
+        
         public ActionResult Edit(int UserID)
         {
             ViewUser model = getUserByID(UserID);
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ViewUser model)
+        {
+            // Edit exist User to Database
+            ModelState.Clear();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // lay file
+                    byte[] img = null;
+                    HttpPostedFileBase file = Request.Files["file"];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        img = new byte[file.ContentLength];
+                        file.InputStream.Read(img, 0, file.ContentLength);
+                    }
+
+                   // xoa role hien tai
+                    Roles.RemoveUserFromRole(model.username, Roles.GetRolesForUser(model.username)[0]);
+                    // Gan Role moi
+                    int roleID = Int32.Parse(model.RoleName);
+                    switch (roleID)
+                    {
+                        case 1:
+                            Roles.AddUserToRole(model.username, "Administrator");
+                            break;
+                        case 2:
+                            Roles.AddUserToRole(model.username, "Manager");
+                            break;
+                        case 3:
+                            Roles.AddUserToRole(model.username, "Staff");
+                            break;
+                    }
+
+                    //Update vao database
+                    using (SqlConnection conn = new SqlConnection(System.Web.Configuration.WebConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString))
+                    {
+                        conn.Open();
+                        string sqlSelect = @"update user set Name = @name, DateOfBirth = @date, Address = @add where username = @username ";
+                        using (SqlCommand cmd = new SqlCommand(sqlSelect, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@name", model.Name);
+                            cmd.Parameters.AddWithValue("@date", model.DateOfBirth);
+                            cmd.Parameters.AddWithValue("@add", model.address);
+                            cmd.Parameters.AddWithValue("@username", model.username);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", ModelState.Values.All(modelState => modelState.Errors.Count == 0).ToString());
+            }
+            return RedirectToAction("List");
         }
     }
 }
